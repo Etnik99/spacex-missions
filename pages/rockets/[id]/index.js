086f6bf3416da64686cb24e6fdc0aa66fdd120c6
etnik-lab <60393820+etnik-lab@users.noreply.github.com>
2022-01-19 15:06:37 +0100

    Style and aplication done

--- pages/rockets/[id]/index.js
+++ pages/rockets/[id]/index.js
@@ -2,19 +2,23 @@ import { gql } from "@apollo/client";
 import client from "../../../apollo-client";
 
 const Rocket = ({ rocket }) => {
-    return (
-        <>
-            <h1>{rocket.name}</h1>
-            <h1>{rocket.type}</h1>
-            <h1>{rocket.description}</h1>
-            <h1>{rocket.country}</h1>
-        </>
-    )
+  return (
+    <>
+      <div className="rocket-infos">
+        <p id='rocket-info'><span>Rocket Country: </span>{rocket.country}</p>
+        <p id='rocket-info'><span>Rocket Name: </span>{rocket.name}</p>
+        <p id='rocket-info'><span>Rocket Type: </span>{rocket.type}</p>
+        <span>Rocket Info:</span>
+        <p id='rocket-description'>{rocket.description}</p>
+      </div>
+
+    </>
+  )
 }
 
 export async function getStaticPaths() {
-    const { data } = await client.query({
-        query: gql`
+  const { data } = await client.query({
+    query: gql`
           query Rockets {
             launchesPast(limit: 10) {
               rocket {
@@ -25,17 +29,17 @@ export async function getStaticPaths() {
             }
           }
         `,
-    });
+  });
 
-    const paths = data.launchesPast.map(({ rocket }) => ({ params: { id: rocket.rocket.id }, }))
+  const paths = data.launchesPast.map(({ rocket }) => ({ params: { id: rocket.rocket.id }, }))
 
-    return { paths, fallback: false };
+  return { paths, fallback: false };
 }
 
 export async function getStaticProps({ params }) {
 
-    const { data } = await client.query({
-        query: gql`
+  const { data } = await client.query({
+    query: gql`
           query Rocket($id: ID!){
             rocket(id: $id) {
                 name
@@ -45,13 +49,13 @@ export async function getStaticProps({ params }) {
             }
           }
         `,
-        variables: { id: params.id },
-    });
+    variables: { id: params.id },
+  });
 
-    return {
-        props: {
-            rocket: data.rocket,
-        }
+  return {
+    props: {
+      rocket: data.rocket,
     }
+  }
 }
 export default Rocket;
